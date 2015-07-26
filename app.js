af412945a000851660d2118c59e03239a440ccc5
Simone Busoli <simone.busoli@gmail.com>
2015-07-26 19:05:24 +0200

    include link in page title

--- app.js
+++ app.js
@@ -6,5 +6,8 @@ require('./models');
 mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost:27017/messa-demo');
 
 messa(mongoose, {
-  title: 'demo app for messa (github.com/simoneb/messa)'
+  title: 'demo app for messa ' +
+  '<a target="_blank" href="https://github.com/simoneb/messa">' +
+  '(https://github.com/simoneb/messa)' +
+  '</a>',
 }).listen(process.env.PORT || 3000);
